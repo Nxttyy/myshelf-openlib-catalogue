4928783edd201dbb6af4047d6436355232707f1b
Nathnael <nathnaelyirga@gmail.com>
2026-06-03 21:27:56 +0300

    [fix]: password encoding before hashing/hash-matching

--- app/auth.py
+++ app/auth.py
@@ -14,13 +14,11 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    # Bcrypt has a 72-byte limit.
-    return pwd_context.verify(plain_password[:72], hashed_password)
+    return pwd_context.verify(plain_password.encode("utf-8")[:72], hashed_password)
 
 
 def get_password_hash(password: str) -> str:
-    # Bcrypt has a 72-byte limit.
-    return pwd_context.hash(password[:72])
+    return pwd_context.hash(password.encode("utf-8")[:72])
 
 
 def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None) -> str:
